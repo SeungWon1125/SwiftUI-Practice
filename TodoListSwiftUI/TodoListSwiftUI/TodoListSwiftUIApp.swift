88451d53fffbc7f69be01de70d3d0a563ebcd531
won <andrew112517@naver.com>
2025-03-08 20:14:48 +0900

    [setting] 프로젝트 생성

---
+++ TodoListSwiftUI/TodoListSwiftUI/TodoListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListSwiftUIApp.swift
+//  TodoListSwiftUI
+//
+//  Created by 김승원 on 3/8/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
