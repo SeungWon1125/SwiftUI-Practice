5477b16258f889cf134758d156b8ccce30947336
won <andrew112517@naver.com>
2025-03-16 14:52:12 +0900

    [setting] 플젝 생성

---
+++ UMC8th/UMC8th/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UMC8th
+//
+//  Created by 김승원 on 3/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UMC8th/UMC8th/UMC8thApp.swift
@@ -0,0 +1,17 @@
+//
+//  UMC8thApp.swift
+//  UMC8th
+//
+//  Created by 김승원 on 3/16/25.
+//
+
+import SwiftUI
+
+@main
+struct UMC8thApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
